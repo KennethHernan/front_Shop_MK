2dfeb477124514441d8a74ba818caf4a2ef59cb3
KennethHernan <kdelacruz18@autonoma.edu.pe>
2025-10-17 14:18:54 -0500

    Correccion 01

--- src/Services/auth.js
+++ src/Services/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const urlBase = import.meta.env.VITE_URL_BASE;
 
-export const postCreatePreference = async () => {
+export const postCreatePreference = async (idOrder, userEmail, items) => {
   const { data } = await axios.post(`${urlBase}/api/create_preference`, {idOrder, userEmail, items});
   return data;
 };
