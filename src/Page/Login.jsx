5d8aebf71f8be644d0bfd0818aa5b4d302f9a959
KennethHernan <kdelacruz18@autonoma.edu.pe>
2025-10-22 10:40:33 -0500

    Update 22/10/2025 - create login

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -20,14 +20,16 @@ function Login() {
 
   async function enviarCodigo(email) {
     try {
-      const res = await fetch("/api/enviarCode", {
+      const res = await fetch("https://checkoutmk.vercel.app/api/enviar-code", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ email }),
       });
-      const data = await res.json();
+      console.log(res);
+      
+      const data =  res;
 
       if (data.ok) {
         console.log("Código enviado:", data.code);
@@ -40,16 +42,22 @@ function Login() {
   }
 
   return (
-    <div className="bg-[#F2D0BD] text-[#000] h-[100vh] w-full flex justify-center items-center">
-      <section className="bg-white p-5 rounded-sm">
-        <p>hola</p>
+    <div className="bg-[#e0e0e060] text-[#000] h-[100vh] w-full flex justify-center items-center select-none">
+      <section className="bg-white p-5 rounded-sm m-5">
+        <p className="w-full text-center text-[25px] my-7 font-light">
+          MAYIKH SYTYLE
+        </p>
         <form onSubmit={handleSubmit(onSubmit)} className="w-full h-full">
-          <div className="flex justify-between w-full mb-2 text-lg">
+          <div className="flex justify-between w-full mb-1 text-lg">
             <p className="font-medium">Iniciar sesión</p>
           </div>
+          <p className="text-[#9d9d9d] text-sm sm:w-[50vh] font-light">
+            Introduce tu correo electrónico y te enviaremos un codigo de
+            verificación.
+          </p>
 
           {/* Input Correo Electronico */}
-          <div className="relative w-full">
+          <div className="relative w-full my-4">
             <input
               type="email"
               id="email"
@@ -61,7 +69,7 @@ function Login() {
                         : "border-[1px] border-gray-300"
                     }
                     `}
-              placeholder="Correo Electrónico"
+              placeholder="Correo electrónico"
               {...register("email", {
                 required: "Introduce un correo electrónico",
               })}
@@ -72,7 +80,7 @@ function Login() {
                bg-[#39333300] px-1 pointer-events-none 
                peer-[&:not(:placeholder-shown)]:top-5 peer-[&:not(:placeholder-shown)]:opacity-100 peer-[&:not(:placeholder-shown)]:text-xs"
             >
-              Correo Electrónico
+              Correo electrónico
             </label>
             {errors.email && (
               <p className="text-[14px] text-red-500 my-2">
@@ -86,13 +94,17 @@ function Login() {
             type="submit"
             disabled={cargando}
             className={`
-                  lg:block hidden w-full text-[15px] my-4 font-medium text-white rounded-lg py-4 bg-blue-700 hover:bg-blue-800 transition-colors duration-300
+                  w-full text-[15px] my-5 font-medium text-white rounded-lg py-3 bg-blue-700 bg-opacity-80 hover:bg-blue-800 transition-colors duration-300
                   ${cargando && "animate-pulse"}
                   `}
           >
-            {!cargando ? "Pagar Ahora" : "Procesando..."}
+            {!cargando ? "Continuar" : "Validando..."}
           </button>
         </form>
+        <div className="flex gap-2 text-xs text-blue-500">
+          <p className="hover:underline">Privacidad</p>
+          <p className="hover:underline">Terminos</p>
+        </div>
       </section>
     </div>
   );
