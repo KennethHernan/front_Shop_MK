56e9ccf1a39fc61456e2c9d61b376d837b14deac
KennethHernan <kdelacruz18@autonoma.edu.pe>
2025-11-20 15:22:05 -0500

    Update 20/11/2025

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCVzLgqhjT6cZgkqxqKPRrUstxqpH8oj9E",
@@ -17,6 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const db = getDatabase(app); 
 
-//export const db = getDatabase(app);
-export { app, auth };
+export { app, auth, db };
